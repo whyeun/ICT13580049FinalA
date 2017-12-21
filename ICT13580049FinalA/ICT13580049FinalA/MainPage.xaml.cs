94393f7f49692b9774538ac5d04a51ff7c824b0f
whyeun <joom_m123@hotmail.com>
2017-12-21 13:48:44 +0700

    แก้3

--- ICT13580049FinalA/ICT13580049FinalA/MainPage.xaml.cs
+++ ICT13580049FinalA/ICT13580049FinalA/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ICT13580049FinalA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,9 +16,38 @@ namespace ICT13580049FinalA
             newButton.Clicked += NewButton_Clicked;
 		}
 
-        private void NewButton_Clicked(object sender, EventArgs e)
+        protected override void OnAppearing()
+        {
+            LoadData();
+        }
+        void LoadData()
+        {
+            productListView.ItemsSource = App.DbHelper.GetProducts();
+        }
+        void NewButton_Clicked(object sender, EventArgs e)
         {
             Navigation.PushModalAsync(new ProductNewPage());
         }
+        void Edit_Clicked(object sender, System.EventArgs e)
+        {
+
+            var button = sender as MenuItem;
+            var product = button.CommandParameter as Product;
+            Navigation.PushModalAsync(new ProductNewPage(product));
+        }
+        async void Delete_Clicked(object sender, System.EventArgs e)
+        {
+            var isOk = await DisplayAlert("ยืนยัน", "คุณต้องการลบใช่หรือไม่", "ใช่", "ไม่ใช่");
+            if (isOk)
+            {
+                var button = sender as MenuItem;
+                var product = button.CommandParameter as Product;
+                App.DbHelper.DeleteProduct(product);
+
+                await DisplayAlert("ลบสำเร็จ", "ลบข้อมูลเรียบร้อย", "ตกลง");
+                LoadData();
+            }
+        }
     }
 }
+
